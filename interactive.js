4b881cb403c2d641bb8c24a9e4a0c7352ebb7480
Adele de Saint Guilhem <dsg.adele@gmail.com>
2024-03-19 18:10:15 +0000

    Added image folder, created individual divs for each piece using js. Issue with the size of the images

--- interactive.js
+++ interactive.js
@@ -1,5 +1,18 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
 
-canvas.style.background = "red";
+// I need to create 12 divs within puzzleContainer, each will contain a piece of the puzzle. It all has to be in order. 
+// Inspired by https://www.youtube.com/watch?v=sp6Di21WVjE&t=0s&ab_channel=CryptersInfotech. Change notably the selector method and append method.
+
+let puzzleContainer = document.getElementById("puzzle-container");
+
+function creatingGrid() {
+    let rows = 3;
+    let columns = 4;
+
+    for(let i=0; i<rows*columns; i++){
+        let pieceDiv = document.createElement('div')
+        pieceDiv.className = `piece${i}`;
+        puzzleContainer.appendChild(pieceDiv);
+    }
+}
+creatingGrid();
 
