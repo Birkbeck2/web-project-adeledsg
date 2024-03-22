bf9683134d7ba02cb61d0e6265af7a0887f2bea1
Adele de Saint Guilhem <dsg.adele@gmail.com>
2024-03-22 17:53:34 +0000

    changed puzzle, made them all fit for 100vh

--- interactive.js
+++ interactive.js
@@ -5,14 +5,28 @@
 let puzzleContainer = document.getElementById("puzzle-container");
 
 function creatingGrid() {
-    let rows = 3;
-    let columns = 4;
+    let rows = 2;
+    let columns = 2;
 
     for(let i=0; i<rows*columns; i++){
         let pieceDiv = document.createElement('div')
         pieceDiv.className = `piece${i}`;
         puzzleContainer.appendChild(pieceDiv);
+        let img = document.createElement("img");
+        img.id = `img${i}`;
+        pieceDiv.appendChild(img);
     }
 }
 creatingGrid();
 
+let img0 = document.getElementById("img0");
+img0.src = "puzzle/Picture1.png"; 
+
+let img1 = document.getElementById("img1");
+img1.src = "puzzle/Picture2.png"; 
+
+let img2 = document.getElementById("img2");
+img2.src = "puzzle/Picture3.png"; 
+
+let img3 = document.getElementById("img3");
+img3.src = "puzzle/Picture4.png"; 
